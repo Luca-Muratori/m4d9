f0a06873bcce22f647f76dd296b0913c1380c16a
Lucai994 <luca.muratori.94@hotmail.it>
2022-02-17 16:59:02 +0100

    commit

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -7,7 +7,7 @@ const MyNav=()=>{
         <Navbar.Brand href="#home"><h1>StriviBook </h1>  </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Nav className="mr-auto ">
-                <Link className='nav-link text-dark' to='/register'>Registration</Link>
+                <Link className='nav-link text-dark ' to='/register'>Registration</Link>
             </Nav>
         </Navbar>
     )
